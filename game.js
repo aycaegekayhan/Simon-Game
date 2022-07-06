fc7cae17078ba7c32068dd64fc67798e929ac1f9
aycaegekayhan <54589042+aycaegekayhan@users.noreply.github.com>
2022-07-06 15:20:55 -0400

    finished game

--- game.js
+++ game.js
@@ -1,24 +1,75 @@
 var buttonColors = ["red", "blue", "green", "yellow"];
 var gamePattern = [];
 var userClickedPattern = [];
+var started = false;
+var level = 0;
+
+$(document).keypress(function() {
+    if(!started) {
+        $("#level-title").text("Level " + level);
+        nextSequence();
+        started = true;
+    }
+});
 
 $(".btn").click(function() {
     var userChosenColor = $(this).attr("id");
     userClickedPattern.push(userChosenColor);
-    console.log(userClickedPattern)
+
+    playSound(userChosenColor);
+    animatePress(userChosenColor);
+    checkAnswer(userClickedPattern.length-1);
 });
 
+function checkAnswer(currentLevel) {
+    if(gamePattern[currentLevel] === userClickedPattern[currentLevel]) {
+        console.log("Success!!!");
+        if(userClickedPattern.length === gamePattern.length){
+            setTimeout(function () {
+                nextSequence();
+            }, 1000);
+        } 
+    } else {
+        console.log("Wrong");
+        playSound("wrong");
+        $("body").addClass("game-over");
+        setTimeout(function () {
+        $("body").removeClass("game-over");
+         }, 200);
+        $("#level-title").text("Game Over, Press any key to restart!");
+        startOver();
+    }
+}
 
 function nextSequence() {
+    userClickedPattern = [];
+    level ++;
+    $("#level-title").text("Level " + level);
     var randomNumber = Math.floor(Math.random()*4);
     var randomChosenColor = buttonColors[randomNumber];
     gamePattern.push(randomChosenColor);
 
     $("#" + randomChosenColor).fadeOut(100).fadeIn(100).fadeOut(100).fadeIn(100);
+    playSound(randomChosenColor);   
+};
 
-    var audio = new Audio("sounds/" + randomChosenColor + ".mp3")
+function startOver() {
+    level = 0;
+    gamePattern = [];
+    started = false;
 }
 
+function playSound(name) {
+    var audio = new Audio("sounds/" + name + ".mp3");
+    audio.play();
+};
+
+function animatePress(currentColor) {
+    $("#" + currentColor).addClass("pressed");
+    setTimeout(function () {
+      $("#" + currentColor).removeClass("pressed");
+    }, 100);
+};
 
 
 
@@ -28,26 +79,6 @@ function nextSequence() {
 
 
 
-
-// $("#green").click(function() {
-//     audioElement.play();
-//     $("#status").text("Status: Playing");
-// });
-
-// $('#red').click(function() {
-//     audioElement.play();
-//     $("#status").text("Status: Playing");
-// });
-
-// $('#yellow').click(function() {
-//     audioElement.play();
-//     $("#status").text("Status: Playing");
-// });
-
-// $('#blue').click(function() {
-//     audioElement.play();
-//     $("#status").text("Status: Playing");
-// });
 
 
 
